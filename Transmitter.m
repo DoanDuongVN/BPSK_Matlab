8d106d8184277c3e0e940159fb3c276622cc9092
Đoàn Dương <56549082+DoanDuongVN@users.noreply.github.com>
2020-12-25 01:43:12 +0700

    Create Transmitter.m

---
+++ Transmitter.m
@@ -0,0 +1,21 @@
+text = dec2bin('Duong Van Doan Tran Xuan Hung ',8);
+text = text';
+text = double(text(:)') - 48;
+np=text;
+
+header=[0 1 1 1 1 1 1 0];
+y=[dongbo np];
+
+y1=kron(ones(20,1),y);
+y1=y1(:);
+
+y2=real(pammod(y1,2));
+
+t=0:2*pi/(10000/1000):10000*pi;
+x=sin(t);
+
+[M,N] = size(y2);
+x=x(1:M);
+plot(x.*y2');
+yphat=(x.*y2');
+sound(yphat,10000,8);
